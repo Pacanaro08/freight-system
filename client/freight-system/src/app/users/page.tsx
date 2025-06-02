69015430f14f103536f2e114c73b15caeeb46a17
Pietro Pacanaro <pietro.macho12@gmail.com>
2025-06-02 20:20:08 -0300

    [new features] filter field and buttons

--- client/freight-system/src/app/users/page.tsx
+++ client/freight-system/src/app/users/page.tsx
@@ -63,8 +63,25 @@ function displayUsers() {
             {
                 screen && (
                     <>
-                        <div className="bg-gray-900 min-h-screen">
-                            <div className="flex justify-center items-center h-full">
+                        <div className="bg-gray-900 min-h-screen flex flex-col">
+                            <div className="bg-gray-700 my-10 min-w-[800px] max-w-4xl w-full mx-auto rounded-lg p-5">
+                                <div className="grid grid-cols-2 h-fit place-items-center gap-x-10">
+                                    <div className="flex justify-center items-center w-full">
+                                        <input
+                                            className='w-full bg-white px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-purple-500 focus:bg-purple-50'
+                                            type='email'
+                                            id='email'
+                                            placeholder='User or E-Mail'
+                                        />
+                                        <button className="px-3 py-2 bg-white rounded border cursor-pointer hover:ring-purple-500 hover:ring-1 hover:bg-purple-50">Search</button>
+                                    </div>
+                                    <div className="w-full grid grid-cols-2 gap-x-10">
+                                        <button className="px-3 py-2 bg-white rounded border w-full cursor-pointer hover:ring-blue-500 hover:ring-1 hover:bg-blue-50">Insert</button>
+                                        <button className="px-3 py-2 bg-white rounded border w-full cursor-pointer hover:ring-green-500 hover:ring-1 hover:bg-green-50">Export</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex justify-center items-center">
                                 {
                                     headers && users && (
                                         <Grid headers={headers} data={users}></Grid>
